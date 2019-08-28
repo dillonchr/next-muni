54f96f133a3a34181b4667350b68fd419026d5b0
Dillon Christensen <dillon@goloansnap.com>
2019-08-28 16:14:53 -0700

    broken but still whole

--- muni.js
+++ muni.js
@@ -11,17 +11,22 @@ const getNextStopTimes = (route, stopId, done) => {
   req.on('error', err => done(err))
 }
 
-const getSecondsUntilNextBus = (bus) => {
-  getNextStopTimes(bus, 6604, (err, res) => {
+const getSecondsUntilNextBus = (bus, stop) => {
+  getNextStopTimes(bus, stop || 6604, (err, res) => {
     if (err) {
       return console.error('Failed to fetch times', err)
     }
+    try {
     const { prediction } = res.predictions.direction
     const seconds = +(Array.isArray(prediction) ? prediction[0] : prediction).seconds
     console.log('Next %d is in %d min %d seconds', bus, ~~(seconds/60), seconds % 60)
+    } catch(err) {
+      console.log(bus, 'failed', err)
+    }
   })
 }
 
 getSecondsUntilNextBus(2)
 getSecondsUntilNextBus(3)
+//getSecondsUntilNextBus(30, 6523)
 
