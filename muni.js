4192a0f3d8aed68c3cf9bfbd83b6f3aae277d768
Dillon Christensen <dillon@goloansnap.com>
2019-08-29 01:16:09 -0700

    updated output for tmux statusbar

--- muni.js
+++ muni.js
@@ -1,33 +1,90 @@
-const http = require('http')
+const fetch = require('@dillonchr/fetch')
+const moment = require('moment')
 
-const getUrl = (route, stopId) => `http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=${route}&s=${stopId}`
+const getUrl = (stops) => stops.reduce((url, {route, stop}) => `${url}&stops=${route}|${stop}`, 'http://webservices.nextbus.com/service/publicJSONFeed?command=predictionsForMultiStops&a=sf-muni')
 
-const getNextStopTimes = (route, stopId, done) => {
-  const req = http.get(getUrl(route, stopId), (res) => {
-    let body = ''
-    res.on('data', chunk => body += chunk)
-    res.on('end', () => done(null, JSON.parse(body)))
-  })
-  req.on('error', err => done(err))
+const getNextStopTimes = (buses, done) => {
+  fetch({url: getUrl(buses)}, done)
 }
 
-const getSecondsUntilNextBus = (bus, stop) => {
-  getNextStopTimes(bus, stop || 6604, (err, res) => {
+const getNextStopFromPrediction = (result) => {
+  const { routeTag: bus, direction } = result
+  if (!direction) {
+    return 0
+  }
+  const dir = Array.isArray(direction) ? direction[0] : direction
+  const { prediction } = dir
+  const nextStop = Array.isArray(prediction) ? prediction[0] : prediction
+  const min = +nextStop.minutes
+  return `🚌${bus} ${min}m`
+}
+
+const getSecondsUntilNextBus = (buses, done) => {
+  getNextStopTimes(buses, (err, res) => {
     if (err) {
-      return console.error('Failed to fetch times', err)
+      return done(err)
     }
     try {
-    const { direction } = res.predictions
-    const { prediction } = Array.isArray(direction) ? direction[0] : direction
-    const seconds = +(Array.isArray(prediction) ? prediction[0] : prediction).seconds
-    console.log('Next %d is in %d min %d seconds', bus, ~~(seconds/60), seconds % 60)
+      let response;
+      if (Array.isArray(res.predictions)) {
+        response = res.predictions
+          .map(getNextStopFromPrediction)
+          .filter(r => !!r)
+          .join(' ')
+      } else {
+        response = getNextStopFromPrediction(res.predictions)
+      }
+      if (!response) {
+        done(null, '🚌 N/A')
+      } else {
+        done(null, response)
+      }
     } catch(err) {
-      console.log(bus, 'failed', err)
+      done(err)
     }
   })
 }
 
-getSecondsUntilNextBus(2)
-getSecondsUntilNextBus(3)
-getSecondsUntilNextBus(30, 6523)
+const replyWithErrorHandling = (reply, err, message) => {
+  if (err) {
+    reply(`Something blew up when fetching predictions: ${err.toString()}`)
+  } else {
+    reply(message)
+  }
+}
+
+const ROUTES = {
+  home: [
+    {route: 2, stop: 6604},
+    {route: 3, stop: 6604}
+  ],
+  pg: [
+    {route: 30, stop: 6523}
+  ],
+  mtg: [
+    {route: 2, stop: 6594},
+    {route: 3, stop: 6594},
+    {route: 47, stop: 6828},
+    {route: 49, stop: 6828}
+  ],
+  union: [
+    {route: 2, stop: 6126},
+    {route: 3, stop: 6126}
+  ],
+  crissy: [
+    {route: 2, stop: 6594},
+    {route: 3, stop: 6594},
+    {route: 47, stop: 6830},
+    {route: 49, stop: 6830},
+    {route: 30, stop: 6801}
+  ]
+}
+
+getSecondsUntilNextBus(ROUTES.home, (err, msg) => {
+  if (err) {
+    console.log(0)
+  } else {
+    console.log(msg)
+  }
+})
 
